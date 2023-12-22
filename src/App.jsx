d1bb35c15edcb874f5d1b9a84aef3b9d1c070729
Mateusz Kujawa | Junior Front End Dev <124597227+MateuszKujawa@users.noreply.github.com>
2023-12-22 15:51:55 +0100

    Update #1
    - Create react app
    - Create Navbar
    - Create Sidebar
    - Create Hero Section
    - Create Animation in Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import Test from "./Test";
+import "./app.scss";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section id="Homepage">
+      <Navbar/>
+      <Hero/>
+    </section>
+    <section id="Usługi">Parallax</section>
+
+    <section >Services</section>
+
+    <section id="Portfolio">Parallax</section>
+
+    <section>Portfolio1</section>
+
+    <section id="Kontakt">Parallax</section>
+
+    <section>Kontakt</section>
+
+    <section id="Omnie">Parallax</section>
+
+    <section>O mnie</section>
+
+    <section id="CV">CV</section>
+    {
+    /* <Test />
+    <Test /> */}
+  </div>;
 };
 
 export default App;
